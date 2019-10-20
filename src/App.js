3830ad87a7b1511df9bd0596d45d44f488be8750
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-10-20 09:14:49 -0400

    added homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard  from './Components/Dashboard';
+import Main from './Components/Main';
+import { Route, BrowserRouter, Switch} from 'react-router-dom';
 
 document.body.style = "background: black;";
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <BrowserRouter>
+      <switch>
+        <Route exact path='/' component={Main}/>
+        <Route exact path='/dashboard' component={ Dashboard}/>
+      </switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Main.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Background from '../earth-horizon.jpg';
+
+class Main extends Component {
+    render() {
+        return (
+            <div style={{ width: '100vw', height: '100vh', backgroundImage: `url(${Background})` }}>
+                <div style={{margin: 'auto', paddingTop: '35vh'}}>
+                <h1 style={{ fontFamily: 'Consolas', color: 'white', fontSize: '6rem'}}>STRATOSCI</h1>
+                <Dropdown>
+                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                        Choose a flight
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="dashboard">Australia</Dropdown.Item>
+                        <Dropdown.Item href="#/dashboard">Timmins</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
